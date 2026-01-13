ce8eac3e37d69f10b0c700444d9339faf56eab32
loopystack <leosoftechstar@gmail.com>
2026-01-12 17:43:15 -0800

    update

--- server/db.js
+++ server/db.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   database: process.env.DB_NAME || '10x_dashboard',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD || 'password',
+  ssl: { rejectUnauthorized: false }
 });
 
 // Test connection

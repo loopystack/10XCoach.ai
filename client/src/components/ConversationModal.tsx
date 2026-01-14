e170e9b6d5db015ad12e911152410a5e42f67861
loopystack <leosoftechstar@gmail.com>
2026-01-14 03:36:17 -0800

    builderror_fixed

--- client/src/components/ConversationModal.tsx
+++ client/src/components/ConversationModal.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from 'react'
 import { X, Mic, Square, Save } from 'lucide-react'
-import { api } from '../utils/api'
 import './ConversationModal.css'
 
 interface Coach {
